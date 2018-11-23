0f60ed29108ccccf286c50f94541dee7ba2fb4ef
Mae Dagum <mayeghen@gmail.com>
2018-11-23 14:00:19 +0800

    Est menu header; added assets

--- src/components/MenuHeaderComponent.js
+++ src/components/MenuHeaderComponent.js
@@ -1,21 +1,28 @@
 import React from 'react';
 
+import add from '../assets/menu/icon-add.svg';
+import filter from '../assets/menu/icon-filter.svg';
 export default function MenuHeaderComponent(props) {
     const {
         handleFilterClick,
         filter_button_class
     }  = props; 
     return (
-        <div>
-        Menu
-        <input 
-            type='text'
-            placeholder='Enter Value'/>
-        <button>FIND</button>
-        <button>Add New</button>
-        <button 
-            className={'class-mo-mae '+filter_button_class}                
-            onClick={() => handleFilterClick()}>FILTERS</button>
-    </div>
+        <div className='menu-header-box'>
+            <div id='text-heading'>Menu</div>
+            <div className='header-search'>
+            <div className='search-box'>
+                <input 
+                    id='input-box'
+                    type='text'
+                    placeholder='Enter menu name'/>
+                <button id='btn-find'>FIND</button>
+            </div>
+            </div>
+            <div className="header-button"><button id='btn-add'><div>Add New</div><img src={add}></img></button>
+            <button 
+                className={'btn-filter' + filter_button_class}                
+                onClick={() => handleFilterClick()}><img src={filter}></img>FILTERS</button></div>
+        </div>
     );
 }
