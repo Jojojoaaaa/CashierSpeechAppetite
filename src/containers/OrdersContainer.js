68ef4a2ed113659c620a1b07897ae632d1d52071
Joahnna Nicole Codilla <joaberrrr@gmail.com>
2018-11-24 13:48:42 +0800

    initial on orders

---
+++ src/containers/OrdersContainer.js
@@ -0,0 +1,21 @@
+import React, {Component}from 'react';
+
+class OrdersContainer extends Component{
+   constructor(props) {
+    super(props);
+   }
+
+   render() {
+       return (
+        <div className='orders-container'>
+            <div className='orders-content'>
+
+            </div>
+            <div>
+            </div>
+        </div>
+       )
+   }
+}
+
+export default OrdersContainer;
